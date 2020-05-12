2c314c5f57ab7c31ea5e7cb92bf0778b9cd149b2
mayar lotfy <47094704+mayarlotfe@users.noreply.github.com>
2020-05-12 18:55:35 +0200

    Add files via upload

---
+++ Hire-without-borders-master/config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    mongoURI: "mongodb://user:1234@hire-without-borders-shard-00-00-h87dx.mongodb.net:27017,hire-without-borders-shard-00-01-h87dx.mongodb.net:27017,hire-without-borders-shard-00-02-h87dx.mongodb.net:27017/test?ssl=true&replicaSet=Hire-without-borders-shard-0&authSource=admin&retryWrites=true&w=majority",
+    secretOrKey: 'verysecretkey'
+
+}
